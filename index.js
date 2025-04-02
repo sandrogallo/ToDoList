974af46aa3172449e32329fd0cf87d33b5d55f85
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2025-04-02 07:42:32 +0000

    start

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var ToDo = require('./todo');
+var list = [];
+list.push( new ToDo("Spesa", "pane, acqua", 3, false, Date.now()) );
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.setHeader('Content-Type', 'text/html');
+  res.write( JSON.stringify(list));
+  res.end();
 })
 
 app.listen(port, () => {
---
+++ todo.js
@@ -0,0 +1,16 @@
+
+module.exports = class ToDo {
+
+    constructor ( title, des, priority, done, date ) {
+        this.title = title;
+        this.des = des;
+        this.priority = priority;
+        this.done = done;
+        this.date = date;
+    }
+
+    toString() {
+        return title + "\n" + des + "\n" + priority + " - " + done + " - " + this.date;
+    }
+
+}
